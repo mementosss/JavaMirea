8b4f91ee32865dbfff44ef15a72255e388b0a88e
mementosss <144599371+mementosss@users.noreply.github.com>
2023-09-11 22:30:12 +0300

    Initial commit

--- src/ru/mirea/lab1/Lab1.java
+++ src/ru/mirea/lab1/Lab1.java
@@ -57,7 +57,7 @@ public class Lab1 {
     public void task5(String[] args) {
 
         for (int i = 0; i < args.length; i++) {
-            System.out.println("Arguments " + (i + 1) + ": " + args[i]);
+            System.out.println("Argument " + (i) + ": " + args[i]);
         }
     }
     public void task6() {
