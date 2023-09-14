62a56a536070cbc4cb77a08c0df2cb7d5666ba37
mementosss <144599371+mementosss@users.noreply.github.com>
2023-09-14 13:38:51 +0300

    commit

--- src/ru/mirea/lab2/TestAuthor.java
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class TestAuthor {
     public static void main(String[] args) {
         Author author = new Author("James", "jjames@gmail.com", 'M');
-
         System.out.println(author);
+
         author.setName("John");
         author.setEmail("johnsmith@example.com"); // Изменение email автора
-        author.setGender('M');
+        author.setGender('W');
 
         System.out.println("Name: " + author.getName());
         System.out.println("Email: " + author.getEmail());
         System.out.println("Gender: " + author.getGender());
 
-
-//        System.out.println("Updated Name: " + author.getName());
-//        System.out.println("Updated Gender: " + author.getGender());
-//        System.out.println("Updated Email: " + author.getEmail());
     }
 }
+
+//        System.out.println("Updated Name: " + author.getName()); //для обновления значений
+//        System.out.println("Updated Gender: " + author.getGender());
+//        System.out.println("Updated Email: " + author.getEmail());
+
