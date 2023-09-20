f894026342c1c3782c255e5a25ab42feac29d86a
mementosss <144599371+mementosss@users.noreply.github.com>
2023-09-20 13:44:08 +0300

    commit

--- src/ru/mirea/lab3/MathxRandom.java
+++ src/ru/mirea/lab3/MathxRandom.java
@@ -44,10 +44,12 @@ public class MathxRandom {
 
         int k = 0;
         for(int i = 0; i < n; i++) {
-            if (array1[i] % 2 == 0) { //если элемент четный
-                array1[k++] = array1[i]; //значение этого элемента копируется в array1[k] и индекс k увеличивается на 1
+            if ((array1[i] % 2 == 0) ) { //если элемент четный
+                array1[k] = array1[i]; //значение этого элемента копируется в array1[k] и индекс k увеличивается на 1
+                k++;
             }
         }
+
         int[] array2 = Arrays.copyOf(array1, k); //создание копии массива с длиной чётных элементов
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
