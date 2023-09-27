dc1b651ce282fd48aef58d03169460c9e7b44575
mementosss <144599371+mementosss@users.noreply.github.com>
2023-09-27 19:02:21 +0300

    commit

--- src/ru/mirea/TFL/Into_ReversePN.java
+++ src/ru/mirea/TFL/Into_ReversePN.java
@@ -7,28 +7,21 @@ import java.util.StringTokenizer;
 public class Into_ReversePN {
     public static String infixToRPN(String expression) {
         StringBuilder result = new StringBuilder();
-        Stack<Character> operatorStack = new Stack<>();// Создаем стек для операторов
+        Stack<Character> operatorStack = new Stack<>();
         // Используем StringTokenizer для разделения выражения на токены (числа и операторы)
         StringTokenizer tokenizer = new StringTokenizer(expression, "()+-*/^", true);
         while (tokenizer.hasMoreTokens()) { // проверяем на наличие следующего токена
             // Получаем следующий токен
             String token = tokenizer.nextToken().trim();
-            if (token.isEmpty()) { // если токены закончились, выходим из цикла
-                continue;
+            if (token.isEmpty()) { // если токены закончились
+                continue; // выходим из цикла
             }
-
-            // Получаем первый символ токена
             char firstChar = token.charAt(0); // возвращает значение char по указанному индексу 0
-
-            // Если токен - число, добавляем его в результат
-            if (Character.isDigit(firstChar) || (token.length() > 1 && firstChar == '-' && Character.isDigit(token.charAt(1)))) {
-                result.append(token).append(" ");
-            } else if (firstChar == '(') {
-            // Если токен - открывающая скобка, помещаем ее в стек
-                operatorStack.push('(');
-            } else if (firstChar == ')') {
-            // Если токен - закрывающая скобка, выталкиваем операторы из стека в результат,
-            // пока не встретим открывающую скобку
+            if (Character.isDigit(firstChar) || (token.length() > 1 && firstChar == '-' && Character.isDigit(token.charAt(1)))) { // Если токен - число
+                result.append(token).append(" "); // добавляем его в результат
+            } else if (firstChar == '(') { // Если токен - открывающая скобка
+                operatorStack.push('('); // помещаем ее в стек
+            } else if (firstChar == ')') { // Если токен - закрывающая скобка выталкиваем операторы из стека в результат
                 while (!operatorStack.isEmpty() && operatorStack.peek() != '(') { // peek (возврат верхнего элемента без его удаления)
                     result.append(operatorStack.pop()).append(" "); // pop (удаление и возврат верхнего элемента стека)
                 }
@@ -39,21 +32,19 @@ public class Into_ReversePN {
             } else {
             // Если токен - оператор, выталкиваем операторы из стека в результат, пока
             // не встретим оператор с меньшим приоритетом или открывающую скобку
-                while (!operatorStack.isEmpty() && precedence(firstChar) <= precedence(operatorStack.peek())) {
-                    result.append(operatorStack.pop()).append(" ");
+                while (!operatorStack.isEmpty() && precedence(firstChar) <= precedence(operatorStack.peek())) { // пока стэк операторов не пуст и пока не встречаем оператор с большим приоритетом
+                    result.append(operatorStack.pop()).append(" "); //добавить извлеченный оператор из стека и добавить его в сторку
                 }
-            // Помещаем текущий оператор в стек
-                operatorStack.push(firstChar);
+                operatorStack.push(firstChar); // Помещаем текущий оператор в стек
             }
         }
-        // Выталкиваем оставшиеся операторы из стека в результат
-        while (!operatorStack.isEmpty()) {
+        while (!operatorStack.isEmpty()) { // пока стэк операторов не пуст
             result.append(operatorStack.pop()).append(" "); //добавить извлеченный оператор из стека и добавить его в сторку
         }
-        return result.toString().trim();// Возвращаем обратную польскую запись как строку, отрезая лишние пробелы
+        return result.toString().trim(); // удвляем лишние пробелы
     }
 
-    private static int precedence(char operator) { // Функция для определения приоритета оператора
+    private static int precedence(char operator) {
         return switch (operator) {
             case '+', '-' -> 1;
             case '*', '/' -> 2;
@@ -66,9 +57,7 @@ public class Into_ReversePN {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Write your expression: ");
         String infixExpression = scanner.nextLine();
-
-        // Вызываем функцию преобразования в обратную польскую запись
-        String rpnExpression = infixToRPN(infixExpression);
+        String rpnExpression = infixToRPN(infixExpression); // Вызываем функцию преобразования в обратную польскую запись
         System.out.println("RPN: " + rpnExpression);
         scanner.close();
     }
